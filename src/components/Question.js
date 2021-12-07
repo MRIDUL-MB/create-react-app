f936792a6b768d6ed7c856d23b702651edb8c265
Mridul Bagla <53037689+MRIDUL-MB@users.noreply.github.com>
2021-12-07 23:52:29 +0530

    completed tap feature

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,53 @@
 import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 export default function Question() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [select, setSelect] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    fetch(
-      "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
-    )
+    fetch("https://opentdb.com/api.php?amount=10")
       .then((res) => res.json())
-      .then((data) => setData(data.results))
+      .then((data) =>
+        setData(
+          data.results.map((ans) => ({
+            answers: [ans.correct_answer, ...ans.incorrect_answers]
+              .sort()
+              .map((item) => ({
+                answer: item,
+                id: nanoid(),
+                isSelected: false
+              })),
+            correct_answer: ans.correct_answer,
+            question: ans.question,
+            id: nanoid()
+          }))
+        )
+      )
       .catch((err) => console.log(err))
       .finally(setLoading(false));
   }, []);
 
   console.log(data);
+
+  function handleSelect(id, parent_id) {
+    setData((prev) => {
+      return prev.map((item) =>
+        item.id === parent_id
+          ? {
+              ...item,
+              answers: item.answers.map((ans) =>
+                ans.id === id
+                  ? { ...ans, isSelected: !ans.isSelected }
+                  : { ...ans, isSelected: false }
+              )
+            }
+          : { ...item }
+      );
+    });
+  }
 
   if (loading) {
     return <h1>Data is Loading!</h1>;
@@ -24,25 +55,24 @@ export default function Question() {
 
   return (
     <div className="questions">
-      <div>
-        {data.map((item, index) => {
-          const { correct_answer, incorrect_answers } = item;
-          const answers = [correct_answer, ...incorrect_answers].sort();
-
-          return (
-            <div key="index">
-              <h3 className="item">{item.question}</h3>
-              <div className="options">
-                {/* {answers.map((answer, i)=>{
-              return <div key={i} className="option"><p>{answer}</p></div>
-                })} */}
-              </div>
-              <br />
-            </div>
-          );
-        })}
-      </div>
-      <button className="btn submit">Check answers</button>
+      {data.map((quiz) => (
+        <div key={quiz.id}>
+          <h3>{quiz.question}</h3>
+          <br />
+          <div>
+            {quiz.answers.map((ans) => (
+              <p
+                className={`option  ${ans.isSelected && "selected"}`}
+                key={ans.id}
+                onClick={() => handleSelect(ans.id, quiz.id)}
+              >
+                {ans.answer}
+              </p>
+            ))}
+          </div>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
